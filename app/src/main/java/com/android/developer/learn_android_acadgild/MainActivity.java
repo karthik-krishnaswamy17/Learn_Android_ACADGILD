00497bd8d41214dc077f8b28ccfcc723d3213961
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-07 13:34:50 +0530

    Sorting Month List using Listview and Adapters.

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,37 +1,78 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    final private String QUERY = "https://www.google.com/search?q=";
-    ImageView img_search;
-    EditText search_query;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String[] ascending_months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct",
+            "Nov", "Dec"
+    };
+    private static final String[] descending_months = {"Dec", "Nov", "Oct", "Sept", "Aug", "July", "June", "May", "Apr", "Mar",
+            "Feb", "Jan"};
+
+    /*
+    Use ArrayList as there would be Run-time Exception if a String is used with clear()/remove() adapter.
+    */
+    private static ArrayList<String> list_months_asce = new ArrayList<>(Arrays.asList(ascending_months));
+    private static ArrayList<String> list_months_desc = new ArrayList<>(Arrays.asList(descending_months));
+
+    private Button bt_ascending, bt_descending;
+    private ListView listView_months;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img_search = (ImageView) findViewById(R.id.search_img);
-        search_query = (EditText) findViewById(R.id.txt_search);
-        img_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = QUERY + (search_query.getText());
-                Intent sendIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                if (sendIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(sendIntent);
-                }
-            }
-
-        });
+        bt_ascending = (Button) findViewById(R.id.ascending);
+        bt_descending = (Button) findViewById(R.id.descending);
+        listView_months = (ListView) findViewById(R.id.list_month);
+        bt_ascending.setOnClickListener(this);
+        bt_descending.setOnClickListener(this);
+        adapter = new ArrayAdapter<>(this, R.layout.simple_list_item, list_months_asce);
+        listView_months.setAdapter(adapter);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.ascending: {
+                Toast.makeText(this, "Sorted in Ascending Order", Toast.LENGTH_SHORT).show();
+                if (adapter != null)
+                    adapter.clear();
+                /*
+                * When the adapter is cleared for the first time,"list_months_asce" gets empty.So this check is added.
+                * */
+                if (list_months_asce.size() == 0)
+                    list_months_asce = new ArrayList<>(Arrays.asList(ascending_months));
 
+                adapter.addAll(list_months_asce);
+                adapter.notifyDataSetChanged();
+
+                break;
+            }
+
+            case R.id.descending: {
+                Toast.makeText(this, "Sorted in Descending Order", Toast.LENGTH_SHORT).show();
+                list_months_desc = new ArrayList<>(Arrays.asList(descending_months));
+                if (adapter != null)
+                    adapter.clear();
+                adapter.addAll(list_months_desc);
+                adapter.notifyDataSetChanged();
+                break;
+            }
+
+        }
+    }
 }
