7b6cbcbd6e217e459fcc913c4148c35f6678b0f8
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-11 12:29:12 +0530

    Option Menu to display txt Color

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,47 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.txt_submenu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.red: {
+                txt.setTextColor(Color.RED);
+                break;
+            }
+
+            case R.id.green: {
+                txt.setTextColor(Color.GREEN);
+                break;
+            }
+
+            case R.id.blue: {
+                txt.setTextColor(Color.BLUE);
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
