08591807efc0f9fb64f711ead52de832591c6942
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-11 20:26:13 +0530

    App to display the Menu Item programmatically.

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,39 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.ToolbarTitle);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitleTextColor(Color.WHITE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.add(R.string.camera);
+        menu.add(R.string.video);
+        menu.add(R.string.email);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String disp = item.getTitle().toString();
+        Toast.makeText(this, disp, Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 }
