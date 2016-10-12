f2acc042305d2d6cb321b0f89cf11a225e9d9e41
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-12 18:50:00 +0530

    ContextMenu App using XML

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,46 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button bt_context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_context = (Button) findViewById(R.id.bt_context);
+        registerForContextMenu(bt_context);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle(R.string.contextTitle);
+        getMenuInflater().inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action1:
+                Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action2:
+                Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action3:
+                Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onContextItemSelected(item);
     }
 }
