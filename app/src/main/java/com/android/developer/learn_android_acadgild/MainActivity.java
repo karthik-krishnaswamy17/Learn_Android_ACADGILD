584d76a36d260ad3ea059b574ed61e481e61e1f6
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-14 11:29:05 +0530

    Fragment APP to load data from EditText

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,32 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt_submit;
+    SimpleAddition fragmentA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_submit = (Button) findViewById(R.id.bt_submit);
+        bt_submit.setOnClickListener(this);
+        fragmentA = (SimpleAddition) getSupportFragmentManager().findFragmentById(R.id.fragment_details);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_details, fragmentA).commit();
+
+    }
+
+    public void onClick(View v) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        String value = editText.getText().toString();
+        textView.setText(value);
     }
 }
---
+++ app/src/main/java/com/android/developer/learn_android_acadgild/SimpleAddition.java
@@ -0,0 +1,23 @@
+package com.android.developer.learn_android_acadgild;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by karthik90 on 10/13/2016.
+ */
+
+public class SimpleAddition extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a, container, false);
+
+    }
+
+}
