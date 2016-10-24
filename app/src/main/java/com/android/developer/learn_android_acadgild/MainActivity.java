6b1389d66bcaec9037c22e1554eba00d20338720
Karthik <karthik.krishnaswamy17@gmail.com>
2016-10-24 21:50:08 +0530

    App dialog  using reveal effect

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,82 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button bt;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.imgCorrect);
+        bt.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        final AlertDialog dialog = new AlertDialog.Builder(this).setView(R.layout.custom_dialog).create();
+        dialog.show();
+        final LinearLayout myView = (LinearLayout) dialog.findViewById(R.id.dialog_container);
+
+        myView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // get the center for the clipping circle
+                int cx = myView.getWidth() / 2;
+                int cy = myView.getHeight() / 2;
+                // get the final radius for the clipping circle
+                float finalRadius = (float) Math.hypot(cx, cy);
+                // create the animator for this view (the start radius is zero)
+                Animator anim =
+                        ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+
+                // make the view visible and start the animation
+                myView.setVisibility(View.VISIBLE);
+                anim.start();
+            }
+        });
+
+        imageView = (ImageView) myView.findViewById(R.id.imgCorrect);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get the center for the clipping circle
+                int cx = myView.getWidth() / 2;
+                int cy = myView.getHeight() / 2;
+                // get the initial radius for the clipping circle
+                float initialRadius = (float) Math.hypot(cx, cy);
+                // create the animation (the final radius is zero)
+                Animator anim =
+                        ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+                // make the view invisible when the animation is done
+
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        myView.setVisibility(View.INVISIBLE);
+                        dialog.dismiss();
+                    }
+                });
+                // start the animation
+                anim.start();
+
+            }
+        });
+    }
+
+
 }
