61e424ea7dbb55d2a7f1f134c6ae1d299a5e60d1
Karthik <karthik.krishnaswamy17@gmail.com>
2016-11-02 13:31:56 +0530

    Android Battery Percentage.

--- app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
+++ app/src/main/java/com/android/developer/learn_android_acadgild/MainActivity.java
@@ -1,13 +1,45 @@
 package com.android.developer.learn_android_acadgild;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView batteryLevel;
+    private BroadcastReceiver myBroadCast = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            String message = "Battery Level Remaining: " + level + "%";
+            batteryLevel.setText(message);
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        batteryLevel = (TextView) findViewById(R.id.battery);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.registerReceiver(myBroadCast, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(myBroadCast);
     }
 }
